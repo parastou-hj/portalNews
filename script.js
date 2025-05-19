66e9d39d1217f43ddf420f7f67c88a8cd0edec2c
parastou-hj <jalilian.parastou91@gmail.com>
2025-05-19 17:03:14 +0330

    scnd-cmt

--- script.js
+++ script.js
@@ -26,3 +26,29 @@
   $(".nav-left").click(function () {
     $("#banner-owl").trigger("next.owl.carousel");
   });
+
+  // Gallery Modal Functionality
+$(document).ready(function() {
+    // Gallery image click handler
+    $('.gallery-item img').on('click', function() {
+        const imageSrc = $(this).attr('src');
+        $('#modalImage').attr('src', imageSrc);
+    });
+    
+    // Tab switching with fade effect
+    $('.nav-link[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+        const target = $(e.target.getAttribute('data-bs-target'));
+        target.hide().fadeIn(300);
+    });
+    
+    // Smooth hover effects for news cards
+    $('.news-card').hover(
+        function() {
+            $(this).find('.news-card-img img').addClass('hover-scale');
+        },
+        function() {
+            $(this).find('.news-card-img img').removeClass('hover-scale');
+        }
+    );
+});
+
